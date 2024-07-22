08035fa7e70272105602ade9b64c2034dbb22be0
Ateto1202 <tonysu1204@gmail.com>
2024-07-22 20:22:56 +0800

    Update some info

--- src/Specialty/Specialty.jsx
+++ src/Specialty/Specialty.jsx
@@ -7,11 +7,11 @@ function Specialty() {
             <div className={styles['specialty-grid']}>
                 <div className={styles['specialty-item']}>
                     <h3>軟體工程</h3>
-                    <p>於大一下開始擔任軟體工程實習生，目前主要使用 Swift 進行 iOS APP 開發，並專注於相機軟體的開發。</p>
+                    <p>熟悉軟體開發流程如 Scrum，於大一開始擔任軟體工程實習生，目前主要使用 Swift 進行 iOS APP 開發，並專注於相機軟體的開發。</p>
                 </div>
                 <div className={styles['specialty-item']}>
                     <h3>物件導向</h3>
-                    <p>包含 SOLID 原則以及設計模式，並基於 OOP 開發出具備低耦合高模組化的架構模式以達成槓桿未來。</p>
+                    <p>熟悉 SOLID 原則以及設計模式，習慣且擅長基於 OOP 開發出具有低耦合高模組化的架構模式以達成槓桿未來。</p>
                 </div>
                 <div className={styles['specialty-item']}>
                     <h3>程式教學</h3>
