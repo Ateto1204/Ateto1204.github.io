714b126a117c51dd4246c109f57dbe3d71cfd07f
Ateto1202 <tonysu1204@gmail.com>
2024-07-29 11:49:54 +0800

    Import theme mode icon

--- src/Button/DarkModeButton.jsx
+++ src/Button/DarkModeButton.jsx
@@ -1,9 +1,21 @@
+import React, { useState } from 'react'
 import styles from './DarkModeButton.module.css'
 
+import moon from '../assets/moon.png'
+import sun from '../assets/sun.png'
+
 function DarkModeButton() {
+    const [theme, setTheme] = useState(sun);
+
+    const toggleTheme = () => {
+        setTheme(theme === sun ? moon : sun)
+    }
+
     return (
         <div className={styles['mode-btn']}>
-            <button>MODE</button>
+            <button onClick={toggleTheme}>
+                <img src={theme} />
+            </button>
         </div>
     );
 }
