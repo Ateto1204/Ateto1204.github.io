2c6292ed90f89750866799fcc43af0ce7a8a2e3c
Ateto1202 <tonysu1204@gmail.com>
2024-07-29 10:34:13 +0800

    Render background image randomly

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import Profile from './Profile/Profile.jsx'
 import Experience from './Experience/Experience.jsx'
 import Skill from './Skill/Skill.jsx'
@@ -6,9 +6,35 @@ import Specialty from './Specialty/Specialty.jsx'
 import Footer from './Footer/Footer.jsx'
 import './App.css'
 
+const images = [
+  '/src/assets/background.JPG',
+  '/src/assets/background2.JPG',
+  '/src/assets/background3.JPG',
+  '/src/assets/background4.JPG',
+];
+
 function App() {
+
+  useEffect(() => {
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+    const root = document.documentElement;
+    root.style.backgroundImage = `url(${randomImage})`;
+    root.style.backgroundPosition = 'center';
+    root.style.backgroundRepeat = 'no-repeat';
+    root.style.backgroundSize = 'cover';
+    root.style.backgroundAttachment = 'fixed';
+
+    return () => {
+      root.style.backgroundImage = '';
+      root.style.backgroundPosition = '';
+      root.style.backgroundRepeat = '';
+      root.style.backgroundSize = '';
+      root.style.backgroundAttachment = '';
+    };
+  }, []);
+
   return (
-    <div class="container">
+    <div className="container">
       <main>
         <Profile />
       </main>
