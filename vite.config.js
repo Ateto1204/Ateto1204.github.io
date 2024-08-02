508f5988616a5e75bf6b61c5efc860cd0e8b6f9c
Ateto1202 <tonysu1204@gmail.com>
2024-08-02 14:38:21 +0800

    Update config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
     resolve: {
     alias: {
       '@': '/src',
+      '@fortawesome/free-regular-svg-icons': path.resolve(__dirname, 'node_modules/@fortawesome/free-regular-svg-icons'),
     },
   }, 
   build: {
