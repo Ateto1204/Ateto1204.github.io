682924f6e75f50988c5588374e60ce1464c1552c
Ateto1202 <tonysu1204@gmail.com>
2024-08-06 09:35:12 +0800

    Fetch github avatar as web title

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import Specialty from './Components/Specialty/Specialty'
 import Footer from './Components/Footer/Footer'
 
 import ThemeModeButton from './Components/Button/ThemeModeButton'
+import FaviconSetter from './Api/FaviconSetter'
 import './App.css'
 
 function App() {
   return (
     <>
+      <FaviconSetter />
       <div className="container">
         <main>
           <Profile />
