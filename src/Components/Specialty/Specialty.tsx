90e69fb4a2f70970023b31dd83f5e1f71cb20689
Ateto1202 <tonysu1204@gmail.com>
2024-08-09 11:18:44 +0800

    Create basic style

--- src/Components/Specialty/Specialty.tsx
+++ src/Components/Specialty/Specialty.tsx
@@ -1,6 +1,4 @@
-import React, { useEffect } from 'react';
-
-import styles from './Specialty.module.css'
+import { useEffect } from 'react';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -17,30 +15,30 @@ function Specialty() {
     }, []);
 
     return (
-        <section className={styles['specialty']} data-aos="fade-up">
-            <div className={styles['specialty-title']}>
+        <section  data-aos="fade-up">
+            <div className='flex flex-row justify-center'>
                 <FontAwesomeIcon icon={faCodeCompare} size="xl" />
-                <h1>Specialties</h1>
+                <h1 className='pl-2'>Specialties</h1>
             </div>
-            <div className={styles['specialty-grid']}>
-                <div className={styles['specialty-item']}>
-                    <div className={styles['specialty-item-title']}>
+            <div className='border-2 border-gray-700 rounded-3xl grid grid-cols-3 p-8 m-8'>
+                <div className='flex flex-col flex-wrap m-2'>
+                    <div className='flex flex-row p-2'>
                         <FontAwesomeIcon icon={faCodePullRequest} />
-                        <h3>軟體工程</h3>
+                        <h3 className='pl-2 items-baseline'>軟體工程</h3>
                     </div>
                     <p>熟悉軟體開發流程如 Scrum，於大一開始擔任軟體工程實習生，目前主要使用 Swift 進行 iOS APP 開發，並專注於相機軟體的開發。</p>
                 </div>
-                <div className={styles['specialty-item']}>
-                    <div className={styles['specialty-item-title']}>
+                <div className='flex flex-col flex-wrap m-2'>
+                    <div className='flex flex-row p-2'>
                         <FontAwesomeIcon icon={faCubes} />
-                        <h3>物件導向</h3>
+                        <h3 className='pl-2 items-baseline'>物件導向</h3>
                     </div>
                     <p>熟悉 SOLID 原則以及設計模式，習慣且擅長基於 OOP 開發出具有低耦合且高模組化特性的架構模式以達成槓桿未來。</p>
                 </div>
-                <div className={styles['specialty-item']}>
-                    <div className={styles['specialty-item-title']}>
+                <div className='flex flex-col flex-wrap'>
+                    <div className='flex flex-row p-2'>
                         <FontAwesomeIcon icon={faBook} />
-                        <h3>程式教學</h3>
+                        <h3 className='pl-2 items-baseline'>程式教學</h3>
                     </div>
                     <p>自高中至今多次以講師身份教授程式設計相關課程，我擅長換位思考找出學生的學習痛點，由此可知我是一個有同理心的人。</p>
                 </div>
@@ -49,4 +47,4 @@ function Specialty() {
     );
 }
 
-export default Specialty
+export default Specialty;
