6ada95518b0baa54e27706ad0e2925fb1909939e
Ateto1202 <tonysu1204@gmail.com>
2024-08-09 10:49:26 +0800

    Import tailwind and refactor footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,10 +1,11 @@
-import styles from './Footer.module.css'
+// import styles from './Footer.module.css'
+import '../../index.css';
 
 function Footer() {
     let currentYear = new Date().getFullYear();
     return (
-        <section className={styles['footer']}>
-          <p>© Copyright ©{currentYear} All rights reserved</p>
+        <section className="mt-44 mb-4">
+          <p className="text-center text-gray-500 opacity-75">© Copyright ©{currentYear} All rights reserved</p>
         </section>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './main.css';
+// import './index.css';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
