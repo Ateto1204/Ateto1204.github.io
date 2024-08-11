204a62dc19c3437e73346eb0028e6ff6ba9bd40c
Ateto1202 <tonysu1204@gmail.com>
2024-08-11 10:51:28 +0800

    Complete specialty page

--- src/Components/Specialty/Specialty.tsx
+++ src/Components/Specialty/Specialty.tsx
@@ -15,32 +15,32 @@ function Specialty() {
     }, []);
 
     return (
-        <section className=' flex flex-wrap justify-center flex-col items-center px-44 py-8' data-aos="fade-up">
-            <div className='flex flex-row justify-center'>
-                <FontAwesomeIcon icon={faCodeCompare} size="xl" />
-                <h1 className='pl-2'>Specialties</h1>
+        <section className='flex flex-wrap justify-center flex-col items-center px-8 py-8' data-aos="fade-up">
+            <div className='flex justify-center items-center pb-[70px]'>
+                <FontAwesomeIcon icon={faCodeCompare} size='xl' />
+                <h1 className='font-serif font-semibold text-6xl pl-5 m-0'>Specialties</h1>
             </div>
-            <div className='border-2 border-gray-700 rounded-3xl grid grid-cols-3 p-8 m-8'>
+            <div className='grid gap-20 border shadow-[5px_5px_20px] p-8 rounded-3xl border-solid border-[gray] grid-cols-3 w-fit'>
                 <div className='flex flex-col flex-wrap m-2'>
-                    <div className='flex flex-row p-2'>
+                    <div className='flex flex-row items-baseline pb-[10px]'>
                         <FontAwesomeIcon icon={faCodePullRequest} />
-                        <h3 className='pl-2 items-baseline'>軟體工程</h3>
+                        <h3 className='font-sans text-2xl pl-2 font-semibold items-baseline'>軟體工程</h3>
                     </div>
-                    <p>熟悉軟體開發流程如 Scrum，於大一開始擔任軟體工程實習生，目前主要使用 Swift 進行 iOS APP 開發，並專注於相機軟體的開發。</p>
+                    <p className='leading-8 m-0'>熟悉軟體開發流程如 Scrum，於大一開始擔任軟體工程實習生，目前主要使用 Swift 進行 iOS APP 開發，並專注於相機軟體的開發。</p>
                 </div>
                 <div className='flex flex-col flex-wrap m-2'>
-                    <div className='flex flex-row p-2'>
+                    <div className='flex flex-row items-baseline pb-[10px]'>
                         <FontAwesomeIcon icon={faCubes} />
-                        <h3 className='pl-2 items-baseline'>物件導向</h3>
+                        <h3 className='font-sans text-2xl pl-2 font-semibold items-baseline'>物件導向</h3>
                     </div>
-                    <p>熟悉 SOLID 原則以及設計模式，習慣且擅長基於 OOP 開發出具有低耦合且高模組化特性的架構模式以達成槓桿未來。</p>
+                    <p className='leading-8 m-0'>熟悉 SOLID 原則以及設計模式，習慣且擅長基於 OOP 開發出具有低耦合且高模組化特性的架構模式以達成槓桿未來。</p>
                 </div>
                 <div className='flex flex-col flex-wrap'>
-                    <div className='flex flex-row p-2'>
+                    <div className='flex flex-row items-baseline pb-[10px]'>
                         <FontAwesomeIcon icon={faBook} />
-                        <h3 className='pl-2 items-baseline'>程式教學</h3>
+                        <h3 className='font-sans text-2xl pl-2 font-semibold items-baseline'>程式教學</h3>
                     </div>
-                    <p>自高中至今多次以講師身份教授程式設計相關課程，我擅長換位思考找出學生的學習痛點，由此可知我是一個有同理心的人。</p>
+                    <p className='leading-8 m-0'>自高中至今多次以講師身份教授程式設計相關課程，我擅長換位思考找出學生的學習痛點，由此可知我是一個有同理心的人。</p>
                 </div>
             </div>
         </section>
