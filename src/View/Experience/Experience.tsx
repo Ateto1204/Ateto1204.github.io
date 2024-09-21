86a3cbe84636f34d3f211144a7120d49d5c04b1f
Ateto1202 <tonysu1204@gmail.com>
2024-09-21 23:48:45 +0800

    Handle layout on phone screen

--- src/View/Experience/Experience.tsx
+++ src/View/Experience/Experience.tsx
@@ -22,16 +22,16 @@ const TimelineItem = ({
 }: TimelineItemProps) => {
     return (
         <li className='overflow-hidden relative m-0'>
-            <p className='text-sm md:text-base float-left font-semibold w-[70px] md:w-[170px] mt-4 mr-5 md:mr-0'>
+            <p className='text-base lg:float-left font-semibold w-48 mt-4 mr-5 md:mr-0'>
                 {date}
             </p>
             <div 
-                className='w-3/5 float-left border-l-4 border-solid pl-8 py-4
-                            before:w-3 before:h-3 before:rounded-full before:bg-gray-400 
-                            before:absolute before:left-[86px] md:before:left-[166px] before:top-[24px]'
+                className='w-auto lg:w-3/5 float-left lg:border-l-4 border-solid pl-8 py-4
+                            lg:before:w-3 lg:before:h-3 before:rounded-full before:bg-gray-400 
+                            before:absolute lg:before:left-[188px] before:top-[24px]'
             >
                 <div 
-                    className='cursor-pointer pl-4 py-2 hover:opacity-60 transition-opacity
+                    className='cursor-pointer pl-4 pr-4 py-2 hover:opacity-60 transition-opacity
                                 shadow-[1px_1px_10px] border rounded-2xl border-[rgb(184,190,197)]'
                     onClick={() => alert(alertMessage)}
                 >
