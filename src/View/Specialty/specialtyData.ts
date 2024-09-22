6ae03a6f2af7a18a645ddf80d8f9a701333c9f19
Ateto1202 <tonysu1204@gmail.com>
2024-09-22 13:45:59 +0800

    Abstraction specialty page

---
+++ src/View/Specialty/specialtyData.ts
@@ -0,0 +1,20 @@
+import { faBook, faCodePullRequest, faCubes } from '@fortawesome/free-solid-svg-icons';
+
+
+export const specialties = [
+    {
+        icon: faCodePullRequest,
+        title: "軟體工程",
+        description: "熟悉軟體開發流程與模式如 Scrum 以及專案管理，於大一開始擔任軟體工程實習生，目前主要使用 Swift 進行 iOS APP 開發。",
+    },
+    {
+        icon: faCubes,
+        title: "物件導向",
+        description: "熟悉 SOLID 原則以及設計模式，習慣且擅長基於 OOP 開發低耦合、高內聚、模組化的架構模式如乾淨架構，以達到可擴展性與可測試性。",
+    },
+    {
+        icon: faBook,
+        title: "程式教學",
+        description: "高中至今累積豐富程式設計相關教學經驗，我擅長換位思考找出學生的學習痛點，由此可知我是一個有同理心的人。",
+    }
+];
