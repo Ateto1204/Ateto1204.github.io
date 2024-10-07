383944a8839576a99a2cc4da0ff0860b1af235a2
Ateto1202 <tonysu1204@gmail.com>
2024-10-07 21:58:28 +0800

    Add cursor pointer to profile avatar

--- src/component/profile/ProfileAvatar.tsx
+++ src/component/profile/ProfileAvatar.tsx
@@ -19,7 +19,7 @@ function ProfileAvatar() {
             className={`relative transform-style-3d transition duration-300
                         h-36 w-36 mb-3 mt-12 mr-6 
                         md:h-52 md:w-44 lg:h-80 lg:w-56 lg:float-left
-                        xl:h-96 xl:w-80 xl:mr-16`}
+                        xl:h-96 xl:w-80 xl:mr-16 cursor-pointer`}
         >
             <div className='card'>
                 <img 
