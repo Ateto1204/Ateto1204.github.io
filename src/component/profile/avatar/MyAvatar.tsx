fc95aac4c8447a16c5dff4c2b5db6d62500d7c93
Ateto1202 <tonysu1204@gmail.com>
2024-11-23 09:47:17 +0800

    Modify profile img layout

--- src/component/profile/avatar/MyAvatar.tsx
+++ src/component/profile/avatar/MyAvatar.tsx
@@ -6,9 +6,9 @@ function MyAvatar() {
             <img 
                 src={avatarURL}
                 alt={name}
-                className='h-36 object-cover shadow-none rounded-lg
+                className='h-36 w-28 pb-1 px-1 object-cover shadow-none rounded-lg
                             md:h-52 md:w-64 lg:h-72 2xl:h-80 
-                            hover:shadow-2xl transition-shadow pb-3 bg-white'
+                            hover:shadow-2xl transition-shadow md:pb-3 bg-white'
             />
         </div>
     );
--- src/component/profile/avatar/ProfileAvatar.tsx
+++ src/component/profile/avatar/ProfileAvatar.tsx
@@ -13,9 +13,9 @@ function ProfileAvatar({ cardRef, handleClick, imgUrl}: ProfileAvatarProps) {
             ref={cardRef}
             onClick={handleClick}
             className={`relative transform-style-3d transition duration-300
-                        h-36 w-36 mb-3 mt-12 mr-6 
-                        md:h-52 md:w-44 lg:h-80 lg:w-56 lg:float-left
-                        xl:h-96 xl:w-80 xl:mr-16 cursor-pointer`}
+                        h-36 w-36 mb-3 mt-5 mr-6 
+                        md:h-52 md:w-44 lg:h-80 lg:w-60 lg:float-left
+                        xl:h-96 xl:w-80 xl:mx-8 cursor-pointer`}
         >
             <MyAvatar />
             <MbtiAvatar imgUrl={imgUrl} />
