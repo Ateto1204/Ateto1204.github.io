1a050c8b9659817b906958ed5224734384e5ae93
Ateto1202 <tonysu1204@gmail.com>
2025-06-21 13:32:40 +0800

    Hitfix

--- tests/playwright/profile.spec.ts
+++ tests/playwright/profile.spec.ts
@@ -1,62 +1,70 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://ateto1204.github.io/');
+test("has title", async ({ page }) => {
+    await page.goto("https://ateto1204.github.io/");
 
-  await expect(page).toHaveTitle(/Dong-Yi Su/);
+    await expect(page).toHaveTitle(/Dong-Yi Su/);
 });
 
-test('has profile img', async ({ page }) => {
-  await page.goto('https://ateto1204.github.io/');
+test("has profile img", async ({ page }) => {
+    await page.goto("https://ateto1204.github.io/");
 
-  await page.getByRole('img', { name: '蘇東毅' }).click();
+    await page.getByRole("img", { name: "蘇東毅" }).click();
 });
 
-test('has title name and profile subtitle', async({ page }) => {
-  await page.goto('https://ateto1204.github.io/');
+test("has title name and profile subtitle", async ({ page }) => {
+    await page.goto("https://ateto1204.github.io/");
 
-  await page.getByRole('heading', { name: 'Dong-Yi Su' }).click();
-  await page.getByRole('heading', { name: 'Software Engineer | Developer' }).click();
+    await page.getByRole("heading", { name: "Dong-Yi Su" }).click();
+    await page
+        .getByRole("heading", { name: "Software Engineer | Developer" })
+        .click();
 
-  await page.locator('header svg').first().click();
-  await page.locator('header path').nth(1).click();
+    await page.locator("header svg").first().click();
+    await page.locator("header path").nth(1).click();
 });
 
-test('has profile text paragraph', async ({ page }) => {
-  await page.goto('https://ateto1204.github.io/');
+test("has profile text paragraph", async ({ page }) => {
+    await page.goto("https://ateto1204.github.io/");
 
-  await page.getByText('This is Dong-Yi Su aka Ateto').click();
-  await page.getByText('My domains of expertise include').click();
+    await page.getByText("This is Dong-Yi Su aka Ateto").click();
+    await page.getByText("My domains of expertise include").click();
 });
 
-test('has contact icon', async ({ page }) => {
-  await page.goto('https://ateto1204.github.io/');
+test("has contact icon", async ({ page }) => {
+    await page.goto("https://ateto1204.github.io/");
 
-  const page1Promise = page.waitForEvent('popup');
-  await page.getByRole('link').nth(1).click();
-  const page1 = await page1Promise;
-  
-  const mailtoLink = await page.getByRole('link').nth(2).getAttribute('href');
-  expect(mailtoLink).toContain('mailto:');
+    const page1Promise = page.waitForEvent("popup");
+    await page.getByRole("link").nth(1).click();
+    const page1 = await page1Promise;
 
-  const page3Promise = page.waitForEvent('popup');
-  await page.getByRole('link').nth(3).click();
-  const page3 = await page3Promise;
+    const mailtoLink = await page.locator('a[href^="mailto:"]').first();
+    const href = await mailtoLink.getAttribute("href");
+    expect(href).toContain("mailto:");
+
+    const page3Promise = page.waitForEvent("popup");
+    await page.getByRole("link").nth(3).click();
+    const page3 = await page3Promise;
 });
 
-test('exp page', async ({ page }) => {
-  await page.goto('https://ateto1204.github.io/');
+test("exp page", async ({ page }) => {
+    await page.goto("https://ateto1204.github.io/");
 
-  await page.locator('section').filter({ hasText: 'Experience Jul2024 -' }).locator('path').nth(0).click();
-  await page.getByRole('heading', { name: 'Experience' }).click();
+    await page
+        .locator("section")
+        .filter({ hasText: "Experience Jul2024 -" })
+        .locator("path")
+        .nth(0)
+        .click();
+    await page.getByRole("heading", { name: "Experience" }).click();
 
-  await page.locator('.w-9 > path:nth-child(2)').click();
-  await page.locator('.w-9 > path').click();
+    await page.locator(".w-9 > path:nth-child(2)").click();
+    await page.locator(".w-9 > path").click();
 });
 
-test('has language switching btn', async ({ page }) => {
-  await page.goto('https://ateto1204.github.io/');
-  
-  await page.getByRole('button', { name: '中' }).click();
-  await page.getByRole('button', { name: 'En' }).click();
+test("has language switching btn", async ({ page }) => {
+    await page.goto("https://ateto1204.github.io/");
+
+    await page.getByRole("button", { name: "中" }).click();
+    await page.getByRole("button", { name: "En" }).click();
 });
